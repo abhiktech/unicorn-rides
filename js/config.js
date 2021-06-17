7857e1336a911afb9988e05d3427345040116a9c
abhiktech <abhikmaz@gmail.com>
2021-06-16 22:06:40 -0700

    Deployed backend REST API

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_Gh1ECmeMZ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6ouju4qm3b9qdhpafrni0237nv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-west-2_Gh1ECmeMZ',
+        userPoolClientId: '6ouju4qm3b9qdhpafrni0237nv',
+        region: 'us-west-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://jfqi3ljy0f.execute-api.us-west-2.amazonaws.com/prod'
     }
 };
