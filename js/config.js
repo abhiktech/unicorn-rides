96104fad8c9facc05634ddaa801cfbf974f9b3af
abhiktech <abhikmaz@gmail.com>
2021-06-16 21:09:23 -0700

    Added Cognito credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_Gh1ECmeMZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ouju4qm3b9qdhpafrni0237nv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
